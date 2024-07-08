06c5d3eb27100a77459100f55413aa4443dd7627
smirdch183 <90450824+smirdch183@users.noreply.github.com>
2024-07-08 15:41:41 +0300

    Add files via upload

---
+++ Cofix/Cofix/FolderClass/ClassMB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Cofix.FolderClass
+{
+    internal class ClassMB
+    {
+        public static void MBerror(Exception text)
+        {
+            MessageBox.Show(text.Message, "Ошибка",
+            MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public static void MBerror(string text)
+        {
+            MessageBox.Show(text, "Ошибка",
+            MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public static void MBinformation(string textMessage)
+        {
+            MessageBox.Show(textMessage, "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public static bool QuestionMessage(string text)
+        {
+            return MessageBoxResult.Yes == MessageBox.Show(text, "Вопрос",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+        }
+        public static void ExitMessageBox()
+        {
+            bool resultMB = QuestionMessage("Вы действительно желаете выйти?");
+            if (resultMB == true)
+            {
+                App.Current.Shutdown();
+            }
+        }
+    }
+}
